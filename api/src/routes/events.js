5f9ac27f18bc096faa566231d45e421cfb806f4b
Markanight <marcosvila96@gmail.com>
2022-12-11 17:00:29 -0300

    route query

--- api/src/routes/events.js
+++ api/src/routes/events.js
@@ -19,6 +19,24 @@ route.get("/",async(req,res)=>{         // GET http://localhost:3001/events
 
 });
 
+route.get("/",async(req,res)=>{         // GET http://localhost:3001/events
+
+    try {
+        const {name} = req.query
+        const event = await getEvents();
+
+        const searchEvent = event.filter((event) => event.name === name) 
+
+        res.status(200).json(searchEvent);
+
+    } catch (error) {
+
+        res.status(500).send(error.message);
+        
+    };
+
+});
+
 route.get("/:id",async(req,res)=>{         // GET http://localhost:3001/events
 
     try {
