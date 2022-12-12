50f701ae8593bdf37c7b5b3020ce968af70f7210
Nicolas Morales <morales.nicolas98@hotmail.com>
2022-12-12 01:30:26 -0300

    Working on components

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import SearchBar from '../SearchBar/SearchBar'
 import './navbar.css'
 
 export default function Navbar() {
   return (
     <div className='navbar'>
-       <div className='left'>
-        <h1>Eventix</h1>
+      <div className='left'>
+          <a href="/">
+        <h1>
+            Eventix
+        </h1>
+          </a>
         <h3>About us</h3>
         <h3>More</h3>
-        <SearchBar/>
+        <SearchBar />
       </div>
       <div className='right'>
         <button>Log In</button>
