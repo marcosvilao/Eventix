62f950d1dd88c87abc605216fa1b572800f6e609
Markanight <marcosvila96@gmail.com>
2022-12-12 22:14:45 -0300

    update components styles

---
+++ client/src/components/About Us/AboutUs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './aboutUs.css'
+
+export default function aboutUs() {
+  return (
+        <p className='aboutUs'>Eventix is an event marketplace whith better functionalities, cheaper servicies, and simple tu use. Be free to filter the events like you want </p>  
+         
+  )
+}
--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -5,14 +5,12 @@ import './card.css'
 
 export default function Card({event}) {
   return (
-    <div className='countryCard'>
-      
+    <div className='eventCard'>
         <Link to={'/home/' + event.id}>
           <img src={event.image} alt={event.name} width={230} height={145} />
           <h3>{event.name}</h3>
           <h5>{event.date}</h5>
         </Link>
-      
     </div>
   )
 }
