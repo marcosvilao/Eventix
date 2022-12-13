c662e2f2d57f0682b42b8629c5552da1a45047b2
Nicolas Morales <morales.nicolas98@hotmail.com>
2022-12-13 19:41:34 -0300

    Implementing Login system

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom'
 import Home from './components/Home/Home';
 import Detail from './components/Detail/Detail';
-import { Profile } from './components/Navbar/Login/Profile';
 
 
 
@@ -13,7 +12,6 @@ function App() {
       <Switch>
       <Route exact path={'/'} component={Home}/>
       <Route exact path={'/home/:id'} component={Detail}/>
-      <Route exact path={'/profile'} component={Profile}/>
     </Switch>
     </div>
     
