00d17a9749fb99c81adc9bf7987f193299f0da93
Nicolas Morales <morales.nicolas98@hotmail.com>
2022-12-13 19:40:10 -0300

    Implementing Login system

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SearchBar from '../SearchBar/SearchBar'
-import './navbar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import { LoginButton } from "./Login/LoginButton";
+import {Profile} from './Login/Profile'
+
+import SearchBar from "../SearchBar/SearchBar";
+import "./navbar.css";
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
-      <div className='left'>
-          <a href="/">
-        <h1>
-            Eventix
-        </h1>
-          </a>
+    <div className="navbar">
+      <div className="left">
+        <a href="/">
+          <h1>Eventix</h1>
+        </a>
         <h3>About us</h3>
         <h3>More</h3>
         <SearchBar />
       </div>
-      <div className='right'>
-        <button>Log In</button>
-        <button>Register</button>
+      <div className="right">
+        <LoginButton />
       </div>
     </div>
-  )
+  );
 }
