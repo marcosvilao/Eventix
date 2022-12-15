f15230d11d3748c9fdf412dac413eab6743b8eed
Nicolas Morales <morales.nicolas98@hotmail.com>
2022-12-15 15:07:44 -0300

    lot of changes

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,25 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AboutUs from '../About Us/AboutUs'
 import Grid from '../Grid/Grid'
 import Navbar from '../Navbar/Navbar'
+import { useAuth0 } from '@auth0/auth0-react';
+import { useHistory } from "react-router-dom";
 import './home.css'
 
 
 
 function Home() {
+  const [isVerified, setIsVerified] = useState(false)
+  const history = useHistory()
+  const { isAuthenticated, user } = useAuth0();
 
-
-  return (
+  const emailVerified = user?.email_verified
+  
+  if(isAuthenticated && !emailVerified){
+    return (
     <div>
-        <Navbar/>
-        <AboutUs/>
-        <div className='grid'>
-         <Grid/> 
-        </div>
-        
-    </div>
-
+  <p>
+   "please verify your email"
+    </p>
+  <button onClick={() => history.go(0)}>RELOAD</button>
+   </div>
+   )
+  }else return (
+    <div>
+         <Navbar/>
+         <AboutUs/>
+         <div className='grid'>
+          <Grid/> 
+         </div>
+     </div>
   )
+
+  // return info ? (
+  //   <div>
+  //       <Navbar/>
+  //       <AboutUs/>
+  //       <div className='grid'>
+  //        <Grid/> 
+  //       </div>
+        
+  //   </div>
+
+  // ) : (<div>
+  //   <p>
+  //   "please verify your email"
+  //   </p>
+  //   <button onClick={() => history.go(0)}>RELOAD</button>
+  // </div>)
 }
 
 export default Home
--- client/src/components/Navbar/Login/Profile.jsx
+++ client/src/components/Navbar/Login/Profile.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useHistory } from "react-router-dom";
 
 export const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
+const history = useHistory()
+  const { loginWithRedirect } = useAuth0();
 
-  return (
-    isAuthenticated ? (
+  return isAuthenticated ? (
+    <div>
+      <button onClick={() => history.goBack()}>back</button>
       <div>
-        <div><img src={user.picture} alt="" /></div>
-        <h2>{user.name}</h2>
-        {JSON.stringify(user)}
+        <img src={user.picture} alt="" />
       </div>
-    )
-    :
-    "No iniciaste sesion"
-    
-  );
+      <h2>{user.name}</h2>
+      {JSON.stringify(user)}
+    </div>
+  ) : loginWithRedirect();
 };
