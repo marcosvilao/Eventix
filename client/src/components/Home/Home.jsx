bea1fc0b995e3184f086cef686ff2629d812f90a
Nicolas Morales <morales.nicolas98@hotmail.com>
2022-12-15 16:17:00 -0300

    Refactorizing login

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,47 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import AboutUs from '../About Us/AboutUs'
-import Grid from '../Grid/Grid'
-import Navbar from '../Navbar/Navbar'
-import { useAuth0 } from '@auth0/auth0-react';
+import React, { useEffect, useState } from "react";
+import AboutUs from "../About Us/AboutUs";
+import Grid from "../Grid/Grid";
+import Navbar from "../Navbar/Navbar";
+import { useAuth0 } from "@auth0/auth0-react";
 import { useHistory } from "react-router-dom";
-import './home.css'
-
-
+import "./home.css";
 
 function Home() {
-  const [isVerified, setIsVerified] = useState(false)
-  const history = useHistory()
-  const { isAuthenticated, user } = useAuth0();
+  const history = useHistory();
+  const { isAuthenticated, user, logout } = useAuth0();
 
-  const emailVerified = user?.email_verified
-  
-  if(isAuthenticated && !emailVerified){
+
+function handleClick(){
+logout()
+return history.push("/")
+}
+  // if(isAuthenticated && !emailVerified){
+  //   return (
+  //   <div>
+  // <p>
+  //  "please verify your email"
+  //   </p>
+  // <button onClick={() => history.go(0)}>RELOAD</button>
+  //  </div>
+  //  )
+  // }else
+  if (/unauthorized/.test(window.location.href)) {
     return (
     <div>
-  <p>
-   "please verify your email"
-    </p>
-  <button onClick={() => history.go(0)}>RELOAD</button>
-   </div>
-   )
-  }else return (
+      <p>A verifitacion email has been sent to your email. After verification <a style={{color:"white", textDecoration:"underline"}} href="/profile">click here</a> to continue :D</p>
+      <p>¿Can´t verify? Start over by <button onClick={() => handleClick()}>Clicking here</button></p>
+    </div>);
+  }
+  return (
     <div>
-         <Navbar/>
-         <AboutUs/>
-         <div className='grid'>
-          <Grid/> 
-         </div>
-     </div>
-  )
+      <Navbar />
+      <AboutUs />
+      <div className="grid">
+        <Grid />
+      </div>
+    </div>
+  );
 
   // return info ? (
   //   <div>
   //       <Navbar/>
   //       <AboutUs/>
   //       <div className='grid'>
-  //        <Grid/> 
+  //        <Grid/>
   //       </div>
-        
+
   //   </div>
 
   // ) : (<div>
@@ -52,4 +60,4 @@ function Home() {
   // </div>)
 }
 
-export default Home
+export default Home;
