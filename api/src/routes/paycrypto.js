13ca610cbb6ca734008d70eaa477b30259cd1833
MaxMdr2022 <maxi_meder94@hotmail.com>
2022-12-22 22:30:46 -0300

    pasarela coinbase

---
+++ api/src/routes/paycrypto.js
@@ -0,0 +1,70 @@
+const {Router} = require("express");
+require('dotenv').config();
+const {COINBASE_API_KEY, DOMAIN} = process.env;
+const {Client, resources} = require("coinbase-commerce-node"); // resources son los servicios que ofrece coinbase. Si queremos crear una orden de pago, lo inficamos en resources
+
+
+
+Client.init(COINBASE_API_KEY);  // conectamos a coinbase
+
+const {Charge} = resources;  // charge: Crear ordende pago (le indicamos a coinbase cuanto queremos cobrar y por que producto ). checkout: un producto a un precio definido. Event: 
+
+const route = Router();
+
+
+
+route.post("/create-charge", async(req,res)=>{   // ruta de pago http://localhost:3001/paycrypto/create-charge
+
+    
+    const {total,name,description} = await req.body;
+    // console.log(total,name, description);
+
+    const chargeData = {                    //  datos del pago
+        name: name,
+        description: description, 
+        local_price: {
+            amount: total,
+            currency: 'USD'
+        },
+        pricing_type: 'fixed_price',
+        metadata: {                        // info del comprador. para guardar en la base de datos.
+            customer_id: "idusuaro",
+            customer_name: "Maxi Meder"
+        },
+        redirect_url: `${DOMAIN}/success-payment`, // cuando el pago se finaliza le sale un boton para continuar. Esa url es donde lo va a redirigir el boton. NOTA: tiene que ser un dominio https. Si no coinbase no redirecciona. 
+        cancel_url: `${DOMAIN}/cancel-payment`,  // cuando se calcele el pago, va a redireccionar a esta ruta.
+    };
+
+    const charge = await Charge.create(chargeData);  // le pasamos los datos para que cree la orden de pago. charge es un json con toda la info. donde vamos a tomar el parametro hosted_url que contiene la url que nos manda a la pasarela de pago
+    
+    // res.send(charge); //  podemos ver el json con todos los parametros entrando en http://localhost:3001/paycrypto/create-charge
+
+    const url = charge.hosted_url;  // guardamos la url de la pasarela
+
+    console.log("url::::",url);
+
+    res.redirect(url); // redireccionamos a la url de la pasarela de pago. 
+   
+});
+
+
+route.get("/succes-payment", async (req,res)=>{ // que redireccione a una pag cuando el pago se confirme
+
+    res.send("payment succesfull");
+
+    // res.redirect("http://localhost:3001/user"); // que redireccione al perfil del usuario dnde va a poder ver los tickets comprados.
+});
+
+route.get("/cancel", async(req,res)=>{ // lo mismo pero cuando el pago se cancele. 
+ 
+    res.send("cancel payment");
+    // res.redirect("http://localhost:3001/user");
+});
+
+module.exports = route;
+
+
+//DOCUMENTACION: 
+//https://docs.cloud.coinbase.com/sign-in-with-coinbase/docs/client-libraries
+//https://www.npmjs.com/package/coinbase-commerce-node
+//https://www.youtube.com/watch?v=MmSzVCrP3U8
--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect }from 'react';
+import React, { useEffect, useState }from 'react';
 import { searchEventById } from '../../Redux/actions';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import Map from '../Map/Map';
-
+import { payCrypto } from '../../Redux/actions';
 
 
 export default function Detail() {
@@ -11,7 +11,7 @@ export default function Detail() {
   const history = useHistory()
   const dispatch = useDispatch()
   const { id } = useParams()
-
+  const [cantidad, setCantidad] = useState(1);
 
   useEffect(() => {
     dispatch(searchEventById(id))
@@ -24,35 +24,108 @@ export default function Detail() {
   }
   
 
+  function submitData (e){
+
+    if(e.precio === "Entrada Liberada"){
+
+      return history.push("/");
+    };
+
+    let arr = [];
+
+    arr.push( e.tipoDeTicket, "$ "+ e.precio,"cantidad: "+ cantidad + " ticket", eventShowed[0].date[0],"Ubicacion: "+ eventShowed[0].location)
+
+    if(arr.join(" ").length > 200 ){
+
+      arr = [];
+
+      arr.push(  "$ "+ e.precio,"cantidad: "+ cantidad + " ticket", eventShowed[0].date[0])
+    };
+
+
+    console.log(arr.join(" ").length);
+
+    const datosPago = {
+      
+      total: (Number(e.precio) * cantidad / 335).toPrecision(3),
+      name: eventShowed[0].name,
+      description: arr.join(" "),
+
+      // img: eventShowed[0].image,
+      // user:
+      // id_user:
+    };
+
+    return dispatch(payCrypto(datosPago));
+
+  };
+
+  
+  function buttonRest (){
+    
+    if(cantidad > 1){
+      return setCantidad(cantidad - 1);
+    };
+  };
+
+  function buttonSum (){
+
+    return setCantidad( cantidad +1);
+  };
+
+
   return (
     <div>
+
       <div>
         <button onClick={handleBack}>
           BACK
         </button>
       </div>
+
       <div>
         <div>
           <img src={eventShowed.length ? eventShowed[0].image : null} alt= "" />
         </div>
+
         <div>
+
+          
           <p>{eventShowed.length ? eventShowed[0].name : null}</p>
           <p>{eventShowed.length ? eventShowed[0].location : null}</p>
+
+
          {
-          eventShowed.length ? eventShowed[0].price?.map((e, i) => 
+          eventShowed[0].price.length > 0? eventShowed[0].price.map((e, i) => 
             <div key={i}>
-              <p>{e.tipoDeTicket}</p>
-              <p>{e.precio}</p>
-            </div> 
-          ) :
-          null
+              
+              <p>Type Ticket: {e.tipoDeTicket}</p>
+              {e.precio === "Entrada Liberada" ? <p>Price: Free</p> :<p>Price: ${Number(e.precio) * cantidad} | U$D {(Number(e.precio) * cantidad / 335).toPrecision(3)}</p>}
+              <button onClick={()=>submitData(e)}>comprar</button>
+              <button hidden={cantidad > 1 ? false : true} onClick={()=>buttonRest()}>-</button>
+              <button onClick={()=>buttonSum()}>+</button>
+              {cantidad > 1 ? <span> {cantidad} Tickets</span>: <span> {cantidad} Ticket</span>}
+              
+            </div>
+            
+          ) : 
+          <p>Tickets Sold Out  :´(</p>
          }
         </div>
-        <Map/>
-        <div>
-          <Link to={'/event/sale'}><button>BUY TICKETS</button></Link>
-        </div>
+
       </div>
+      <div>
+        <p>Description event: {eventShowed[0].description}</p>
+      </div>
+
+      <Map/>
+
+      {/* 
+      <div>
+        <Link to={'/event/sale'}><button>BUY TICKETS</button></Link>
+      </div> 
+      */}
+
     </div>
   )
-}
+};
