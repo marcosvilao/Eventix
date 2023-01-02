accd78921b0d726c53d97dbc7109e783a49be181
Nicolas Morales <morales.nicolas98@hotmail.com>
2023-01-02 11:00:15 -0300

    import createEvent

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home/Home";
 import Detail from "./components/Detail/Detail";
 import { Profile } from "./components/Navbar/Login/Profile";
 import aboutUs from './components/About Us/AboutUs';
-import createEvent from './components/createEvent/CreateEvent';
+import createEvent from './components/createEvent/createEvent';
 
 
 function App() {
