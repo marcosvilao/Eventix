d5d35de308e1618d89796313ba07751c3b2cf023
Markanight <marcosvila96@gmail.com>
2023-01-11 12:23:47 -0300

    subiendo deploy

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 const server = require('./src/app.js');
 const { eventsToDb } = require('./src/controllers/getEvents.js');
 const { conn } = require('./src/db.js');
+const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, async () => {
+  server.listen(port, async () => {
     await eventsToDb()
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
